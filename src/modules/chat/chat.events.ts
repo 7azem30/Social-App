746d3240b4bf10459ccb5ea6215eed15dd1fb377
7azem30 <bankai23044@gmail.com>
2025-11-04 00:57:30 +0200

    Update chat.events.ts

--- src/modules/chat/chat.events.ts
+++ src/modules/chat/chat.events.ts
@@ -6,23 +6,30 @@ export class chatEvents {
     constructor(private socket: Socket) { }
 
     sendPrivateMessageEvent() {
-        this.socket.on('send-private-message', (data) => {
+        this.socket.on('send-private-message', (data) => 
+            {
             this.chatServices.sendPrivateMessage(this.socket, data)
         })
     }
+    
     getConversationEvent() {
-        this.socket.on('get-conversation-message', (data) => {
+        this.socket.on('get-conversation-message', (data) => 
+            {
             this.chatServices.getConversationMessage(this.socket, data)
         })
     }
-    userStatusEvents() {
+    
+    userStatusEvents() 
+    {
         this.socket.broadcast.emit("user-online", { userId: this.socket.data.userId });
         this.socket.on("disconnect", () => {
             this.socket.broadcast.emit("user-offline", { userId: this.socket.data.userId });
         });
     }
+    
     typingEvents() {
-        this.socket.on("start-typing", ({ targetUserId }) => {
+        this.socket.on("start-typing", ({ targetUserId }) => 
+            {
             this.socket.to(targetUserId).emit("user-typing", { userId: this.socket.data.userId, typing: true });
         });
         this.socket.on("stop-typing", ({ targetUserId }) => {
