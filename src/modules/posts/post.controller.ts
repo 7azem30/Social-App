5f1554cfb98a5fd7461ecec5c28c41a40542b62e
7azem30 <bankai23044@gmail.com>
2025-11-04 00:55:54 +0200

    Update post.controller.ts

--- src/modules/posts/post.controller.ts
+++ src/modules/posts/post.controller.ts
@@ -4,25 +4,38 @@ import postService from "./services/post.service";
 import { addPostValidator, updatePostValidator } from "../../validators";
 const postController = Router();
 
-//add post
+//------add post
 postController.post('/add-post', authentication, hostUpload().array('files'),validationMiddleware(addPostValidator), postService.addPost)
-// update post
+
+
+// ------update post
 postController.put('/post/:id', authentication, hostUpload().array('files'),validationMiddleware(updatePostValidator), postService.updatePost)
 
-//get all posts
+
+//------get posts
 postController.get('/list-posts', authentication, postService.listHomePosts)
-//get post by id
+
+
+//------get post by id
 postController.get('/post-by-id/:id', authentication, postService.getPostById)
-//get all posts for specific user 
+
+
+//------get posts for this user 
 postController.get('/post-user/:id', authentication, postService.getPostsByUserId)
 
-// send email tags
+
+
+//------ send email tags
 postController.post('/tags/:id', authentication, postService.sendEmailTags)
 
-//freezePost
+
+
+//------freezePost
 postController.post('/freeze-post/:id', authentication, postService.freezePost)
 
-// delete post
+
+
+//------ delete post
 postController.post('/delete-post/:id', authentication, postService.deletePost)
 
 export { postController }
