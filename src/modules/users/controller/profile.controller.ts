f4f1c773cfb1ecd9ec4f3a44e72b5c2ac5739bca
7azem30 <bankai23044@gmail.com>
2025-11-04 00:53:35 +0200

    Update profile.controller.ts

--- src/modules/users/controller/profile.controller.ts
+++ src/modules/users/controller/profile.controller.ts
@@ -4,45 +4,66 @@ import profileService from "../services/profile.service";
 import { updateEmailOtpValidator, updatePasswordValidator, updateProfileValidator } from "../../../validators";
 const profileController = Router();
 
-//update profile
+//-------update profile
 profileController.put('/update-profile', authentication, validationMiddleware(updateProfileValidator), profileService.updateProfile)
 
-//delete profile 
+
+//-----delete profile 
 profileController.delete('/delete-profile', authentication, profileService.deleteAccount)
 
-//get profile data
+
+//-----get profile data
 profileController.get('/get-profile', authentication, profileService.getProfile)
 
-//upload profile pictures
+
+//-----upload profile pictures
 profileController.post('/upload-profile', authentication, hostUpload().single('profile'), profileService.uploadProfilePicture)
 
-//upload cover pictures
+
+
+//-----upload cover pictures
 profileController.post('/cover-profile', authentication, hostUpload().single('profile'), profileService.uploadCoverPicture)
 
-//list all users
+
+
+//-----list all users
 profileController.get('/list-users', profileService.listAllUser)
 
-//send friend ship
+
+
+//------send friend ship
 profileController.post('/send-request', authentication, profileService.sendFriendRequest)
 
-//list friend request
+
+
+//------list friend request
 profileController.get('/friend-requests', authentication, profileService.listRequests)
 
-//respond to friend request
-profileController.patch('/respond-to-friend-request', authentication, profileService.respondToFriendShipRequest)
 
-//create group
+
+//------create group
 profileController.post('/create-group', authentication, profileService.createGroup)
 
-//update email
+
+
+//------respond to friend request
+profileController.patch('/respond-to-friend-request', authentication, profileService.respondToFriendShipRequest)
+
+
+//------update email
 profileController.put('/update-email', authentication, validationMiddleware(updateEmailOtpValidator), profileService.updateSendEmailOtp);
 
-//update password
+
+//------update password
 profileController.put('/update-password', authentication, validationMiddleware(updatePasswordValidator), profileService.updatePassword)
 
-//delete friend request
+
+//------unfriend
+profileController.post('/unfriend', authentication, profileService.unfriend)
+
+
+//------delete friend request
 profileController.post('/delete-request', authentication, profileService.deleteFriendRequest)
 
-//unfriend
-profileController.post('/unfriend', authentication, profileService.unfriend)
+
 export { profileController }
