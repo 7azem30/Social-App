d3ec07f188b3c85e5bfbdee7a7ba60c0c4f12c47
7azem30 <bankai23044@gmail.com>
2025-11-04 00:56:46 +0200

    Update comment.controller.ts

--- src/modules/comments/comment.controller.ts
+++ src/modules/comments/comment.controller.ts
@@ -4,19 +4,27 @@ import commentService from "./services/comment.service";
 import { addCommentValidator, updateCommentValidator } from "../../validators";
 const commentController = Router()
 
-//create comment
+//------create comment
 commentController.post('/add-comment', authentication, validationMiddleware(addCommentValidator), commentService.addComment)
 
-//update comment
+
+//------update comment
 commentController.put('/update-comment', authentication, validationMiddleware(updateCommentValidator), commentService.updateComment)
 
-//get comment with replay
+
+//------get comment with replay
 commentController.get('/get-comment-with-replay/:id', authentication, commentService.getCommentWithReplay)
 
-//freeze comment
+
+
+//------delete comment 
+commentController.post('/delete-comment/:id', authentication, commentService.deleteComment)
+
+
+//------freeze comment
 commentController.post('/freeze-comment/:id', authentication, commentService.freezeComment)
 
-//delete comment 
-commentController.post('/delete-comment/:id', authentication, commentService.deleteComment)
+
+
 
 export { commentController }
