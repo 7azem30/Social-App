64d53057ededa2c43ce8a2b32a4b8b25ce05cc5c
7azem30 <bankai23044@gmail.com>
2025-11-04 00:54:25 +0200

    Update auth.controller.ts

--- src/modules/users/controller/auth.controller.ts
+++ src/modules/users/controller/auth.controller.ts
@@ -4,32 +4,32 @@ import { authentication, validationMiddleware } from "../../../middlewares";
 import { signInValidator, signUpValidator } from "../../../validators";
 const authController = Router();
 
-//signup
+//-----signup
 authController.post('/signUp', validationMiddleware(signUpValidator), authService.signUp);
 
-//confirm email
+//-----confirm email
 authController.post('/confirm', authService.confirmEmail)
 
-//signin
+//-----signin
 authController.post('/login', validationMiddleware(signInValidator), authService.signIn)
 
-//logOut
+//-----logOut
 authController.post('/logout', authentication, authService.logOut)
 
-//forget password
+//-----forget password
 authController.post('/forget-password', authentication, authService.forgotPassword)
 
-//reset password
+//-----reset password
 authController.post('/reset-password', authentication, authService.resetPassword)
 
-// authentication with gmail
-authController.post('/enable-2f', authentication, authService.enableTwoFactorAuth)
+//-----authentication with gmail
 authController.post('/confirm-2f', authentication, authService.confirmTwoFactorAuth)
+authController.post('/enable-2f', authentication, authService.enableTwoFactorAuth)
 
-// block user
+// -----block user
 authController.post('/block-user', authentication, authService.blockUser)
 
-// unblock user
+// -----unblock user
 authController.post('/unblock-user', authentication, authService.unblockUser)
 
 export { authController }
